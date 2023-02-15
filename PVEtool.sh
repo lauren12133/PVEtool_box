7a604215448a84daf429b52e14b3d6688de9e101
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2023-02-15 12:41:37 +0800

    Update PVEtool.sh

--- PVEtool.sh
+++ PVEtool.sh
@@ -1,1 +1,49 @@
+#/bin/bash
 
+# 控制台字体
+red(){
+	echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green(){
+	echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow(){
+	echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+
+back2menu(){
+	green "所选操作执行完成"
+	read -p "请输入“y”退出，或按任意键回到主菜单：" back2menuInput
+	case "$back2menuInput" in
+		y) exit 1 ;;
+		*) menu ;;
+	esac
+}
+
+#更换PVE源默认USTC源
+pve_change_source(){
+  wget https://mirrors.ustc.edu.cn/proxmox/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+  echo "#deb https://enterprise.proxmox.com/debian/pve bullseye pve-enterprise" > /etc/apt/sources.list.d/pve-enterprise.list
+  echo "deb https://mirrors.ustc.edu.cn/proxmox/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
+  green "运行完成"
+	back2menu
+}
+
+#更换debian源默认USTC源
+debian_change_source(){
+mv /etc/apt/sources.list /etc/apt/sources.list.bk
+cat > '/etc/apt/sources.list' << EOF
+deb http://mirrors.ustc.edu.cn/debian stable main contrib non-free
+# deb-src http://mirrors.ustc.edu.cn/debian stable main contrib non-free
+deb http://mirrors.ustc.edu.cn/debian stable-updates main contrib non-free
+# deb-src http://mirrors.ustc.edu.cn/debian stable-updates main contrib non-free
+
+# deb http://mirrors.ustc.edu.cn/debian stable-proposed-updates main contrib non-free
+# deb-src http://mirrors.ustc.edu.cn/debian stable-proposed-updates main contrib non-free
+EOF
+  green "运行完成"
+	back2menu
+}
